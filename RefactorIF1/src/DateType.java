9b3a7143fd0ac65fbf67a44fa96be392c60267a3
RaquelGuerreroPerucho <raquelguerreroperucho@gmail.com>
2023-05-14 21:31:26 +0200

    Cambio 2 sugerencia de IntelliJ

--- RefactorIF1/src/DateType.java
+++ RefactorIF1/src/DateType.java
@@ -6,14 +6,12 @@ public class DateType {
             System.out.println(DataType.gd(0));
         }
         public static String gd(int code) {
-            switch (code) {
-                case 702,1082,1083,1114,1184,1266,12403:
-                    return "DATE";
-                case 20,21,23,24,26,700,701,790,1700,2202,2203,2204,2205,2206,3734,3769,2396:
-                    return "NUMERIC";
-                default:
-                    return "STRING";
-            }
+            return switch (code) {
+                case 702, 1082, 1083, 1114, 1184, 1266, 12403 -> "DATE";
+                case 20, 21, 23, 24, 26, 700, 701, 790, 1700, 2202, 2203, 2204, 2205, 2206, 3734, 3769, 2396 ->
+                        "NUMERIC";
+                default -> "STRING";
+            };
         }
     }
 }
